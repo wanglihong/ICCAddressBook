fbf36f5850e1b544b9843f61b4f4408e7a76599f
Dennis Yang <Dennis@WangmatoMac-Pro.local>
2012-02-28 17:22:16 +0800

    Initial Commit

---
+++ ICCAddressBook/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ICCAddressBook
+//
+//  Created by Dennis Yang on 12-2-28.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
